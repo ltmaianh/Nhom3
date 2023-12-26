7f1492e3835e59717cc09cca8ce972bbeefba4ad
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-12-26 15:11:47 +0700

    update Quanlydiem

--- QLSV/Controllers/QuanlydiemController.cs
+++ QLSV/Controllers/QuanlydiemController.cs
@@ -29,14 +29,12 @@ namespace QLSV.Controllers
         {
             ViewBag.PageSize = new List<SelectListItem>()
             {
-                new SelectListItem() { Value="3", Text="3"},
-                new SelectListItem() { Value="5", Text="5"},
-                new SelectListItem() { Value="10", Text="10"},
-                new SelectListItem() { Value="15", Text="15"},
                 new SelectListItem() { Value="25", Text="25"},
+                new SelectListItem() { Value="30", Text="30"},
+                new SelectListItem() { Value="35", Text="35"},
                 new SelectListItem() { Value="50", Text="50"},
             };
-            int pagesize = (PageSize ?? 3);
+            int pagesize = (PageSize ?? 25);
             ViewBag.psize = pagesize;
             var model = _context.Quanlydiem.ToList().ToPagedList(page ?? 1, pagesize);
             return View(model);
